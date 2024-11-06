3550b256c353b2ae2eda2be416aaeba2c9550267
Solomon <ayomidebanjo02@gmail.com>
2024-11-06 11:03:29 +0100

    first commit

---
+++ src/main/java/tera/TeraApplication.java
@@ -0,0 +1,13 @@
+package tera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeraApplication.class, args);
+	}
+
+}
